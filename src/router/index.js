f76a3ef1630fc34b9c1f74adb3a3dfea19d77539
jcatlett99 <joesph@live.ie>
2024-09-18 16:22:25 +0200

    Initial structure of the archive page with the archive cards

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ArchiveView from "../views/ArchiveView.vue";
 
 const routes = [
   {
@@ -15,6 +16,16 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: function () {
       return import(/* webpackChunkName: "about" */ "../views/AboutView.vue");
+    },
+  },
+  {
+    path: "/archive",
+    name: "archive",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ "../views/ArchiveView.vue");
     },
   },
 ];
---
+++ src/views/ArchiveView.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="archive">
+        <Carousel class="carousel">
+            <div class="card-wrapper">
+                <ArchiveCard v-for="(card, index) in cards" :key="index" :thumbnail="card.thumbnail">
+                    <template v-slot:key-info>
+                        <p><strong>{{ card.keyInfo1 }}</strong> {{ card.value1 }}</p>
+                        <p><strong>{{ card.keyInfo2 }}</strong> {{ card.value2 }}</p>
+                    </template>
+                    <template v-slot:text>
+                        <p>{{ card.text }}</p>
+                    </template>
+                </ArchiveCard>
+            </div>
+        </Carousel>
+    </div>
+</template>
+
+<script>
+import ArchiveCard from "@/components/ArchiveCard.vue";
+
+export default {
+    name: "ArchiveView",
+    components: {
+        ArchiveCard,
+    },
+    data() {
+        return {
+            cards: [
+                {
+                    thumbnail: 'https://via.placeholder.com/300',
+                    keyInfo1: 'Key Info 1:',
+                    value1: 'Value 1',
+                    text: 'This is additional text for card 1.'
+                },
+                {
+                    thumbnail: 'https://via.placeholder.com/300',
+                    keyInfo1: 'Key Info 1:',
+                    value1: 'Value 1',
+                    text: 'This is additional text for card 1.'
+                },
+                {
+                    thumbnail: 'https://via.placeholder.com/300',
+                    keyInfo1: 'Key Info 1:',
+                    value1: 'Value 1',
+                    text: 'This is additional text for card 1.'
+                },
+            ],
+        }
+    }
+};
+</script>
+
+<style scoped>
+.carousel {
+    margin-top: 5%;
+}
+
+.card-wrapper {
+    display: flex;
+    gap: 16px;
+    /* Adjust the spacing between cards */
+}
+</style>
